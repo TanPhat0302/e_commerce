f1c5a47d0c114d6a046377e06dda5c772733d2d7
Phat Mai Nguyen Tan <phatt.maii01@gmail.com>
2024-06-13 20:36:25 +0300

    first commit

---
+++ API/migration.sql
@@ -0,0 +1,23 @@
+﻿CREATE TABLE IF NOT EXISTS "__EFMigrationsHistory" (
+    "MigrationId" TEXT NOT NULL CONSTRAINT "PK___EFMigrationsHistory" PRIMARY KEY,
+    "ProductVersion" TEXT NOT NULL
+);
+
+BEGIN TRANSACTION;
+
+CREATE TABLE "Products" (
+    "Id" INTEGER NOT NULL CONSTRAINT "PK_Products" PRIMARY KEY AUTOINCREMENT,
+    "Name" TEXT NULL,
+    "Description" TEXT NULL,
+    "Price" INTEGER NOT NULL,
+    "PictureUrl" TEXT NULL,
+    "Type" TEXT NULL,
+    "Brand" TEXT NULL,
+    "QuantityInStock" INTEGER NOT NULL
+);
+
+INSERT INTO "__EFMigrationsHistory" ("MigrationId", "ProductVersion")
+VALUES ('20240613160539_InitialCreate', '7.0.3');
+
+COMMIT;
+
